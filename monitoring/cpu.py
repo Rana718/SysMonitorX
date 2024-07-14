9ab5815a461bcf9c75ff3eaffaa5e49edb76c1fc
Rana718 <ranadolui718@gmail.com>
2024-07-14 15:09:13 +0530

    Initial commit

---
+++ monitoring/cpu.py
@@ -0,0 +1,28 @@
+import psutil
+import platform
+import wmi
+
+def get_cpu_info():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    cpu_temps = []
+    try:
+        temps = psutil.sensors_temperatures()
+        if 'coretemp' in temps:
+            cpu_temps = temps['coretemp']
+        elif 'cpu-thermal' in temps:
+            cpu_temps = temps['cpu-thermal']
+    except AttributeError:
+        pass
+    
+    if not cpu_temps and platform.system() == 'Windows':
+        try:
+            w = wmi.WMI(namespace="root\\wmi")
+            temperature_info = w.MSAcpi_ThermalZoneTemperature()
+            for sensor in temperature_info:
+                cpu_temps.append(sensor.CurrentTemperature / 10 - 273.15)
+        except ImportError:
+            pass
+        except wmi.x_wmi as e:
+            print(f"Error querying WMI: {e}")
+    
+    return cpu_usage, cpu_temps
